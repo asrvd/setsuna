88ae0457cb6ef9c6c0ca8b52d3ab9b7489c9f229
asrvd <asheeshh@duck.com>
2023-12-18 13:31:38 +0530

    chore: update site url in email template

--- src/app/components/email-template.tsx
+++ src/app/components/email-template.tsx
@@ -17,7 +17,7 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
     <h1>Hey there, {capsule.user.name}!</h1>
     <p>
       Your time capsule &quot;{capsule.name}&quot; can be opened now! Click{" "}
-      <a href={`https://setsuna.vercel.app/capsules/${capsule.id}`}>here</a> to
+      <a href={`https://setsuna.asrvd.me/capsule/${capsule.id}`}>here</a> to
       open it now.
     </p>
   </div>
